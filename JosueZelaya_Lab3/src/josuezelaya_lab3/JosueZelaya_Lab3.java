00a9dfebeeddcf79da1ac3cb7d25f5fc4a8f666e
Josue Zelaya <josuezelaya99@hotmail.es>
2017-02-03 13:48:28 -0600

    cree el menu

--- JosueZelaya_Lab3/src/josuezelaya_lab3/JosueZelaya_Lab3.java
+++ JosueZelaya_Lab3/src/josuezelaya_lab3/JosueZelaya_Lab3.java
@@ -1,21 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package josuezelaya_lab3;
 
-/**
- *
- * @author usuario
- */
+import java.util.ArrayList;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
 public class JosueZelaya_Lab3 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+
+        String opcion = " ";
+        while (!opcion.equals("d")) {
+            opcion = JOptionPane.showInputDialog("Menu\n"
+                    + "a- Agregar\n"
+                    + "b- Modificar\n"
+                    + "c- Eliminar\n"
+                    + "d- Salir!\n");
+        }
+
     }
-    
+
 }
